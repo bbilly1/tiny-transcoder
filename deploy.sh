892192c1e74406bcd73ec5119affda53e1344597
Simon <simobilleter@gmail.com>
2024-02-06 10:36:18 +0100

    add docker deploy

--- deploy.sh
+++ deploy.sh
@@ -27,10 +27,36 @@ function validate {
 
 function sync_docker {
 
+    if [[ $(git branch --show-current) != 'master' ]]; then
+        echo 'you are not on master, dummy!'
+        return
+    fi
+
+    if [[ $(systemctl is-active docker) != 'active' ]]; then
+        echo "starting docker"
+        sudo systemctl start docker
+    fi
+
+    echo "latest tags:"
+    git tag | tail -n 5 | sort -r
+
+    printf "\ncreate new version:\n"
+    read -r VERSION
+
+    echo "build and push $VERSION?"
+    read -rn 1
+
     # start build
     docker buildx build \
         --platform linux/amd64,linux/arm64 \
-        -t bbilly1/tiny-transcoder --push .
+        -t bbilly1/tiny-transcoder \
+        -t bbilly1/tiny-transcoder:"$VERSION" --push .
+
+    # create release tag
+    echo "commits since last version:"
+    git log "$(git describe --tags --abbrev=0)"..HEAD --oneline
+    git tag -a "$VERSION" -m "new release version $VERSION"
+    git push origin "$VERSION"
 
 }
 
