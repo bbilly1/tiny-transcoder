a423446ed658add4bf8fc2eb4ea891df74aeea56
Simon <simobilleter@gmail.com>
2024-02-20 16:22:57 +0100

    remove log

--- app/static/App.js
+++ app/static/App.js
@@ -4,7 +4,6 @@ import QueueComponent from './components/QueueComponent';
 import ProgressMessage from './components/ProgressMessage';
 
 function App() {
-  console.log('reload');
   return (
     <QueueProvider>
       <div>
